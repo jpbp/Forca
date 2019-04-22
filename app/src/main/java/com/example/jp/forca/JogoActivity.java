c31ceea881e38c9f91df382442b22983aac64833
jpbp <jpbp@estudante.ufla.br>
2019-04-22 19:49:51 -0300

    Initial commit, criação das activitys

---
+++ app/src/main/java/com/example/jp/forca/JogoActivity.java
@@ -0,0 +1,14 @@
+package com.example.jp.forca;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JogoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo);
+    }
+}
