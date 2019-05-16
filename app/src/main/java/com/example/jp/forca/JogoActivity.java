c25efdd4ee90021d9cc8e6e756c72b739712b67e
jpbp <jpbp@estudante.ufla.br>
2019-05-16 20:36:46 -0300

    verificação da palavra

--- app/src/main/java/com/example/jp/forca/JogoActivity.java
+++ app/src/main/java/com/example/jp/forca/JogoActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,7 +22,13 @@ public class JogoActivity extends Activity {
 //    private ArrayList<Questão> arrayObjeto;
 //    private ArrayList<Questão> arrayFilme;
 //    private ArrayList<Questão> arrayTeste;
-    private TextView t;
+    private TextView traços;
+    private EditText letra;
+    private Button btn;
+    private int tamPalavra;
+    private String palavra;
+    private String auxTraço;
+    private TextView paTela;
 
 
     @Override
@@ -36,7 +44,10 @@ public class JogoActivity extends Activity {
 //        arrayEsporte = new ArrayList<>();
 //        arrayObjeto = new ArrayList<>();
 //        arrayFilme = new ArrayList<>();
-        t=findViewById(R.id.txt);
+        traços=findViewById(R.id.txt);
+        letra=findViewById(R.id.letras);
+        paTela=findViewById(R.id.palavrinha);
+
         Questão q1 = new Questão("atomo","particula","facil");
         Questão q2 = new Questão("h20","agua","medio");
         Questão q3 = new Questão("raposa","raw","dificil");
@@ -47,19 +58,35 @@ public class JogoActivity extends Activity {
 //       arrayTeste = new ArrayList<>();
 //        temas.put("ciencias",arrayCiencas);
 
-        String palavra="";
-
-
         for(Questão q: temas.get(tema)){
-            Toast.makeText(this, q == null ? "sim" : "nao", Toast.LENGTH_SHORT).show();
             if(q.getDificuldade().equals(dificuldade)){
                 Toast.makeText(getApplicationContext(),"foi",Toast.LENGTH_LONG).show();
                 palavra = q.getPalavra();
 
             }
         }
-        t.setText(palavra);
-
+        int tamLinha=palavra.length();
+        tamPalavra=tamLinha;
+        auxTraço="";
+        int i=0;
+        for (i=0; i<tamLinha; i++){
+            auxTraço+="_ ";
+        }
+        traços.setText(auxTraço);
+//        final char[] palavraTela = new char [tamPalavra] ;
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String verifica=letra.getText().toString();
+//                int i=0;
+//                for(i=0; i<tamPalavra; i++){
+//                    if(verifica.equals(palavra.charAt(i))){
+//                        palavraTela[i] = verifica.charAt(1);
+//                    }
+//                }
+//                paTela.setText(palavraTela,0,tamPalavra);
+//            }
+//        });
 
 
 
